23da9e0eed82a7377d24a4edb911fddd898a9dce
Cohen-J-Omer <cohen.j.omer@gmail.com>
2022-12-04 14:00:28 +0200

    added function type output for overridden funcs

--- src/vpc/node_provider.py
+++ src/vpc/node_provider.py
@@ -249,7 +249,7 @@ class IBMVPCNodeProvider(NodeProvider):
         return nodes
 
     @log_in_out
-    def non_terminated_nodes(self, tag_filters):
+    def non_terminated_nodes(self, tag_filters)-> List[str]:
         """ 
         returns list of ids of non terminated nodes, matching the specified tags. updates the nodes cache.
         IMPORTANT: this function is called periodically by ray, a fact utilized to refresh the cache (self.cached_nodes).
@@ -320,14 +320,14 @@ class IBMVPCNodeProvider(NodeProvider):
         return [node["id"] for node in res_nodes]
 
     @log_in_out
-    def is_running(self, node_id):
+    def is_running(self, node_id)-> bool:
         """returns whether a node is in status running"""
         with self.lock:
             node = self._get_cached_node(node_id)
             return node["status"] == "running"
 
     @log_in_out
-    def is_terminated(self, node_id):
+    def is_terminated(self, node_id)-> bool:
         """returns True if a node is either not recorded or not in any valid status."""
         with self.lock:
             try:
@@ -337,7 +337,7 @@ class IBMVPCNodeProvider(NodeProvider):
                 return True
 
     @log_in_out
-    def node_tags(self, node_id):
+    def node_tags(self, node_id)-> Dict[str, str]:
         """returns tags of specified node id """
 
         with self.lock:
@@ -361,7 +361,7 @@ class IBMVPCNodeProvider(NodeProvider):
             return self.internal_ip(node_id)
 
     @log_in_out
-    def external_ip(self, node_id):
+    def external_ip(self, node_id)-> str:
         """returns head node's public ip. 
         if use_hybrid_ips==true in cluster's config file, returns the ip address of a node based on its 'Kind'."""
 
@@ -375,7 +375,7 @@ class IBMVPCNodeProvider(NodeProvider):
                 return fip[0]["address"]
 
     @log_in_out
-    def internal_ip(self, node_id):
+    def internal_ip(self, node_id)-> str:
         """returns the worker's node private ip address"""
         node = self._get_cached_node(node_id)
 
@@ -391,7 +391,7 @@ class IBMVPCNodeProvider(NodeProvider):
         return node["network_interfaces"][0].get("primary_ip")['address']
 
     @log_in_out
-    def set_node_tags(self, node_id, tags):
+    def set_node_tags(self, node_id, tags) -> None:
         """
         updates local (file) tags cache. updates in memory cache if node_id and tags are specified 
         Args:
@@ -717,7 +717,7 @@ class IBMVPCNodeProvider(NodeProvider):
                 raise e
 
     @log_in_out
-    def terminate_nodes(self, node_ids):
+    def terminate_nodes(self, node_ids)-> Optional[Dict[str, Any]]:
 
         if not node_ids:
             return
@@ -732,7 +732,7 @@ class IBMVPCNodeProvider(NodeProvider):
             future.result()
 
     @log_in_out
-    def terminate_node(self, node_id):
+    def terminate_node(self, node_id)-> Optional[Dict[str, Any]]:
         """Deletes the VM instance and the associated volume. 
         if cache_stopped_nodes==true in the cluster config file, nodes are stopped instead. """
 
@@ -781,5 +781,5 @@ class IBMVPCNodeProvider(NodeProvider):
         return self._get_node(node_id)
 
     @staticmethod
-    def bootstrap_config(cluster_config):
+    def bootstrap_config(cluster_config)-> Dict[str, Any]:
         return cluster_config
