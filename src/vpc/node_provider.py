24a23ee424bbfe568d9a6e8368a5f3b08e513202
Cohen-J-Omer <cohen.j.omer@gmail.com>
2022-12-06 19:40:46 +0200

    logger fix

--- src/vpc/node_provider.py
+++ src/vpc/node_provider.py
@@ -805,7 +805,7 @@ def _configure_logger():
 
     console_output_handler = logging.StreamHandler()
     console_output_handler.setFormatter(file_formatter)
-    console_output_handler.setLevel(level=os.environ.get("INFO"))
+    console_output_handler.setLevel(logging.INFO)
 
     logger.addHandler(file_handler)
     logger.addHandler(console_output_handler)    
